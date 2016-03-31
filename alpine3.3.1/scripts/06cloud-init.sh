8890931922b25b0b612710d45008fe7aad5e5f1f
Matt Dainty <matt@bodgit-n-scarper.com>
2016-03-31 13:54:45 +0100

    Ensure docker is started before cloud-init

--- alpine3.3.1/scripts/06cloud-init.sh
+++ alpine3.3.1/scripts/06cloud-init.sh
@@ -2,6 +2,10 @@ set -ux
 
 apk add cloud-init
 
+cat >/etc/conf.d/cloud-init-local <<EOF
+rc_after="docker"
+EOF
+
 for i in cloud-config cloud-final cloud-init cloud-init-local ; do
   rc-update add $i
 done
